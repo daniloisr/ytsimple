019d58f5edfaa0ea8132caf9e7dc0a62776d7d6e
Danilo Resende <danilo.isr@gmail.com>
2020-08-01 01:39:49 -0300

    Select videos by channel

--- main.js
+++ main.js
@@ -19,7 +19,11 @@ class Storage {
         JSON.stringify(typeof value === "function" ? await value() : value)
       )
 
-    JSON.parse(localStorage.get(key))
+    return this.get(key)
+  }
+
+  static get(key) {
+    return JSON.parse(localStorage.getItem(key))
   }
 
   static get isCacheExpired() {
@@ -38,37 +42,68 @@ class Storage {
 }
 
 class Video {
-  static add(item) {
-    if (!item.id.videoId) return
+  static get container() { delete this.container; return this.container = document.querySelector('.videos') }
+
+  static addAll(videos, { append = true } = {}) {
+    if (!append) this.container.innerHTML = ''
+    videos.forEach(v => this.add(v))
+  }
+
+  static add(video) {
+    if (!video.id.videoId) return
     const embedEl = `<div class="video">
-      <div class="embeded-video" data-plyr-provider="youtube" data-plyr-embed-id="${item.id.videoId}">
-        <img src="${item.snippet.thumbnails.medium.url}">
+      <div class="embeded-video" data-plyr-provider="youtube" data-plyr-embed-id="${video.id.videoId}">
+        <img src="${video.snippet.thumbnails.medium.url}">
       </div>
     </div>`
-    document.querySelector('#videos').innerHTML += embedEl
+    this.container.innerHTML += embedEl
+  }
+}
+
+class Channel {
+  static add(channel) {
+    const embedEl = `<div class="channel" onclick="videosForChannel('${channel.id}')">
+      <img src="${channel.snippet.thumbnails.default.url}">
+    </div>`
+    document.querySelector('.channels').innerHTML += embedEl
   }
 }
 
 class Youtube {
-  static get url() { return new URL('https://www.googleapis.com/youtube/v3/search') }
-  static channelVideos(channelName) {
-    const url = this.url
+  static async request(path, args) {
+    const url = new URL('https://www.googleapis.com/youtube/v3' + path)
     url.search = new URLSearchParams({
       // safe key for my github page :)
       key: config.ytKey,
-      channelId,
       part: 'snippet',
       maxResults: config.ytMaxResults,
-      order: 'date'
+      ...args
     }).toString()
-    return await Storage.getSet(channelName, () => fetch(url).then(res => res.json()))
+
+    return fetch(url).then(res => res.json())
+  }
+
+  static channelVideos(channelId) {
+    return this.request('/search', { channelId, order: 'date' })
+  }
+
+  static channel(id) {
+    return this.request('/channels', { id })
   }
 }
 
 window.main = async function main() {
-  await Promise.all(config.channels.map(async ([channelId, channelName]) => {
-    const res = Youtube.channelVideos(channelName)
-    res.items.forEach(item => Video.add(item))
+  await Promise.all(config.channels.map(async ([id, _channelName]) => {
+    const [channel, videos] = await Storage.getSet(
+      id,
+      () => {
+        return Promise.all([
+          Youtube.channel(id).then(res => res.items[0]),
+          Youtube.channelVideos(id).then(res => res.items)
+        ])
+      })
+    Channel.add(channel)
+    Video.addAll(videos)
   }))
 
   let active = { player: undefined, el: undefined }
@@ -99,4 +134,9 @@ window.main = async function main() {
   document.querySelectorAll('.video').forEach(el => {
     el.addEventListener('click', clickFn(el))
   })
+}
+
+window.videosForChannel = function videosForChannel(id) {
+  const [_channel, videos] = Storage.get(id)
+  Video.addAll(videos, { append: false })
 }
