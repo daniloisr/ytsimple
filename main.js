d68db689ed8cf2cb46b4c9a77117254e563ad1d4
Danilo Resende <danilo.isr@gmail.com>
2020-08-01 00:12:45 -0300

    Code improments

--- main.js
+++ main.js
@@ -1,53 +1,76 @@
-async function withCache(key, fn) {
-  let cache = localStorage.getItem(key)
-  if (cache)
-    return JSON.parse(res)
-
-  let res = await fn()
-  localStorage.setItem(key, JSON.stringify(res))
-  return res
+// https://developers-dot-devsite-v2-prod.appspot.com/youtube/v3/docs/search/list
+const config = {
+  cacheTime: 12 * 60 * 60 * 1000, // 12 hours
+  ytKey: 'AIzaSyCtlQ5JH_RlPQrAkZHPqz0PQVwYpI-MIvE',
+  ytMaxResults: 50,
+  channels: [
+    ['UC3n0qf54OPWei0bVF4W60Gw', 'science-gadgets'],
+    ['UCKHhA5hN2UohhFDfNXB_cvQ', 'manual-do-mundo'],
+  ]
 }
 
-function addVideo(item) {
-  if (!item.id.videoId) return
-  const embedEl = `<div class="video">
-    <div class="embeded-video" data-plyr-provider="youtube" data-plyr-embed-id="${item.id.videoId}">
-      <img src="${item.snippet.thumbnails.medium.url}">
-    </div>
-  </div>`
-  document.querySelector('#videos').innerHTML += embedEl
+class Storage {
+  // get a valor from storage
+  // if cache is valid the value isn't calculated
+  static async getSet(key, value) {
+    if (this.isCacheExpired)
+      localStorage.setItem(
+        key,
+        JSON.stringify(typeof value === "function" ? await value() : value)
+      )
+
+    JSON.parse(localStorage.get(key))
+  }
+
+  static get isCacheExpired() {
+    delete this.isCacheExpired // memoized getter https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/get
+
+    // check if "_refresh" param is present
+    if (new URLSearchParams(window.location.search).get('_refresh'))
+      return this.isCacheExpired = true
+
+    // if cache was set half day ago
+    if (new Date(localStorage.getItem('cache-date')) < new Date() - config.cacheTime)
+      return this.isCacheExpired = true
+
+    return this.isCacheExpired = false
+  }
 }
 
-const channels = [
-  ['UC3n0qf54OPWei0bVF4W60Gw', 'science-gadgets'],
-  ['UCKHhA5hN2UohhFDfNXB_cvQ', 'manual-do-mundo'],
-]
+class Video {
+  static add(item) {
+    if (!item.id.videoId) return
+    const embedEl = `<div class="video">
+      <div class="embeded-video" data-plyr-provider="youtube" data-plyr-embed-id="${item.id.videoId}">
+        <img src="${item.snippet.thumbnails.medium.url}">
+      </div>
+    </div>`
+    document.querySelector('#videos').innerHTML += embedEl
+  }
+}
 
-function cleanLS() {
-  const urlParams = new URLSearchParams(window.location.search)
-  const refresh = urlParams.get('refresh')
-
-  if (refresh !== undefined) localStorage.clear()
+class Youtube {
+  static get url() { return new URL('https://www.googleapis.com/youtube/v3/search') }
+  static channelVideos(channelName) {
+    const url = this.url
+    url.search = new URLSearchParams({
+      // safe key for my github page :)
+      key: config.ytKey,
+      channelId,
+      part: 'snippet',
+      maxResults: config.ytMaxResults,
+      order: 'date'
+    }).toString()
+    return await Storage.getSet(channelName, () => fetch(url).then(res => res.json()))
+  }
 }
 
 window.main = async function main() {
-  cleanLS()
-
-  await Promise.all(channels.map(async ([channelId, channelName]) => {
-    const url = new URL('https://www.googleapis.com/youtube/v3/search')
-    url.search = new URLSearchParams({
-      // safe key for my github page :)
-      key: 'AIzaSyCtlQ5JH_RlPQrAkZHPqz0PQVwYpI-MIvE',
-      channelId,
-      part: 'snippet',
-      maxResults: 50,
-      order: 'date'
-    }).toString()
-
-    const res = await withCache(channelName, () => fetch(url).then(res => res.json()))
-
-    res.items.forEach(item => addVideo(item))
+  await Promise.all(config.channels.map(async ([channelId, channelName]) => {
+    const res = Youtube.channelVideos(channelName)
+    res.items.forEach(item => Video.add(item))
   }))
+
   let active = { player: undefined, el: undefined }
 
   const clickFn = el => {
