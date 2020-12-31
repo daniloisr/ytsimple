b54868d2acb7058ca1a56ef97c5372fe1271afd2
Danilo Resende <danilo.isr@gmail.com>
2020-12-31 19:35:22 -0300

    Add simple cache

--- src/App.js
+++ src/App.js
@@ -1,51 +1,8 @@
 import React from 'react'
 import Plyr from 'plyr'
+import * as yt from './Youtube'
 import './App.css'
 import 'plyr/dist/plyr.css'
-
-// cache
-// channel_id, date => cached items
-
-const config = {
-  cacheTime: 12 * 60 * 60 * 1000, // 12 hours
-  ytKey: process.env.REACT_APP_YT_KEY,
-  ytMaxResults: 50,
-  channels: [
-    ['UC3n0qf54OPWei0bVF4W60Gw', 'science-gadgets'],
-    ['UCKHhA5hN2UohhFDfNXB_cvQ', 'manual-do-mundo'],
-    ['UCn9Erjy00mpnWeLnRqhsA1g', 'ciencia-todo-dia']
-  ]
-}
-
-class Youtube {
-  static async request(path, args) {
-    const url = new URL('https://www.googleapis.com/youtube/v3' + path)
-    url.search = new URLSearchParams({
-      key: config.ytKey,
-      part: 'snippet',
-      maxResults: config.ytMaxResults,
-      ...args
-    }).toString()
-
-    return fetch(url).then(res => res.json())
-  }
-
-  static channel(id) { return this.request('/channels', { id }) }
-  static channelVideos(channelId) { return this.request('/search', { channelId, order: 'date', type: 'video' }) }
-}
-
-async function fetchYoutube() {
-  const promises = config.channels.map(async ([id, _channelName]) =>
-    Promise.all([
-      Youtube.channel(id).then(res => res.items[0]),
-      Youtube.channelVideos(id)
-        .then(res => { if (res.error) { throw res.error.message } else return res.items })
-    ])
-  )
-
-  return (await Promise.all(promises))
-    .reduce((acc, [channel, videos]) => [...acc, { ...channel, videos: videos }], [])
-}
 
 function App() {
   const [activeVideo, setActiveVideo] = React.useState({ video: undefined, player: undefined })
@@ -70,7 +27,7 @@ function App() {
   React.useEffect(() => {
     async function boot() {
       try {
-        const channels = await fetchYoutube()
+        const channels = await yt.fetchChannels()
         if (channels.length) setChannel(channels[0])
         setChannels(channels)
       } catch (exception) {
---
+++ src/Youtube.js
@@ -0,0 +1,52 @@
+const cacheKey = 'ytsimple-channels'
+const cache = JSON.parse(localStorage.getItem(cacheKey) || '{}')
+function cacheAdd(entry) {
+  localStorage.setItem(cacheKey, JSON.stringify(Object.assign(cache, entry)))
+}
+
+async function request(path, args) {
+  const url = new URL('https://www.googleapis.com/youtube/v3' + path)
+  url.search = new URLSearchParams({
+    key: process.env.REACT_APP_YT_KEY,
+    part: 'snippet',
+    ...args
+  }).toString()
+
+  return fetch(url).then(res => res.json())
+}
+
+const enabledChannels = [
+  ['UC3n0qf54OPWei0bVF4W60Gw', 'science-gadgets'],
+  ['UCKHhA5hN2UohhFDfNXB_cvQ', 'manual-do-mundo'],
+  ['UCn9Erjy00mpnWeLnRqhsA1g', 'ciencia-todo-dia']
+]
+
+export function fetchChannels() {
+  const promises = enabledChannels.map(([id, _channelName]) => fetchChannelAndVideos(id))
+  return Promise.all(promises)
+}
+
+async function fetchChannelAndVideos(id) {
+  let cached = cache[id]
+  if (cached) return Promise.resolve(cached)
+
+  const [channel, videos] = await Promise.all([
+    getChannel(id)
+      .then(res => { if (res.error) { throw res.error.message } else return res.items[0] }),
+    getChannelVideos(id)
+      .then(res => { if (res.error) { throw res.error.message } else return res.items })
+  ])
+
+  channel.videos = videos
+  cacheAdd({ [id]: channel })
+
+  return channel
+}
+
+export function getChannel(id) {
+  return request('/channels', { id })
+}
+
+export function getChannelVideos(id) {
+  return request('/search', { channelId: id, order: 'date', type: 'video', maxResults: 50 })
+}
