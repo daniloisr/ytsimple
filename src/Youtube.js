13707d4b5bbc12cec43d7c926fc922c7a303829d
Danilo Resende <danilo.isr@gmail.com>
2021-01-02 12:11:23 -0300

    Add more channels

--- src/Youtube.js
+++ src/Youtube.js
@@ -1,8 +1,9 @@
+const cacheVersion = '1'
 const cacheKey = 'ytsimple-channels'
 const cache = JSON.parse(localStorage.getItem(cacheKey) || '{}')
-function cacheAdd(entry) {
-  localStorage.setItem(cacheKey, JSON.stringify(Object.assign(cache, entry)))
-}
+const cacheAdd = entry => setCache(Object.assign(cache, entry, { version: cacheVersion }))
+const setCache = cache => localStorage.setItem(cacheKey, JSON.stringify(cache))
+// if (cache.version != cacheVersion) setCache(cache = {})
 
 async function request(path, args) {
   const url = new URL('https://www.googleapis.com/youtube/v3' + path)
@@ -17,11 +18,15 @@ async function request(path, args) {
 
 const enabledChannels = [
   ['UCKHhA5hN2UohhFDfNXB_cvQ', 'manual-do-mundo'],
-  ['UCn9Erjy00mpnWeLnRqhsA1g', 'ciencia-todo-dia'],
   ['UCcXhhVwCT6_WqjkEniejRJQ', 'wintergatan'],
   ['UC-adUJnjdrRnRlOJGoDtTqw', 'show-da-luna'],
+  ['UCA6Roeo-qFVk3jvjAdag7Uw', 'cocorico'],
   ['UCtEy9dWyDez3P19XPMEpcaA', 'kukis-trains'],
+  ['UCQptNV_pmHU5UNOeX0duOkg', '04clemea'],
+  ['UCOJe7abm8byWMR3JUeWk8BQ', 'bidone'],
   ['UC3n0qf54OPWei0bVF4W60Gw', 'science-gadgets'],
+  ['UCfBeGUOYM23ZQnepxtRqDYw', 'invenções-historicas'],
+  ['UCn9Erjy00mpnWeLnRqhsA1g', 'ciencia-todo-dia'],
 ]
 
 export function fetchChannels() {
