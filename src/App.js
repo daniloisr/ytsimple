a4328b6d4467e286177948f60d94ac136717ac1b
Danilo Resende <danilo.isr@gmail.com>
2021-03-24 00:09:27 -0300

    Update localStorage usage to fix data limit

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import * as yt from './Youtube'
 import Video from './Video'
 import './App.css'
 
-const loadMoreCache = JSON.parse(localStorage.getItem('loadMoreCache') || '{}')
-const lastLoadCache = JSON.parse(localStorage.getItem('lastLoadCache') || '{}')
-
 function App() {
   const [activeVideo, setActiveVideo] = React.useState({ video: undefined, player: undefined })
   const [error, setError] = React.useState('')
@@ -14,7 +11,6 @@ function App() {
   const [channels, setChannels] = React.useState([])
   const [loading, setLoading] = React.useState(false)
   const [canLoadMore, setCanLoadMore] = React.useState(false)
-  const updateLoadMore = channel => setCanLoadMore(!(loadMoreCache[channel.id] === false) && channel.videos.length >= 50)
 
   function stopPlayer() { if (activeVideo.player) activeVideo.player.destroy() }
 
@@ -24,79 +20,29 @@ function App() {
   }
 
   async function selectChannel(channel) {
-    if (Date.now() - lastLoadCache[channel.id] > 24 * 60 * 60 * 1000) {
-      const res = await yt.getChannelVideos(channel.id, { after: channel.videos[0].snippet.publishedAt })
-      if (res.error) {
-        setError(res.error.message)
-        return
-      }
-
-      lastLoadCache[channel.id] = Date.now()
-      localStorage.setItem('lastLoadCache', JSON.stringify(lastLoadCache))
-
-      if (res.items.length > 1) {
-        // ignores the last result, because it's the video used on
-        // { after: channel.videos[0].snippet.publishedAt })
-        channel.videos.unshift(...res.items.slice(0, -1))
-        yt.cacheAdd({ [channel.id]: channel })
-      }
-    }
-
     stopPlayer()
     setChannel(channel)
-    setVideos(channel.videos)
-    updateLoadMore(channel)
+    setVideos(await channel.videos())
+    setCanLoadMore(channel.canLoadMore())
   }
 
   async function loadMore() {
     if (loading) return
     setLoading(true)
 
-    const res = await yt.getChannelVideos(channel.id, { before: videos[videos.length - 1].snippet.publishedAt })
-    setLoading(false)
-    if (res.error) {
-      setError(res.error.message)
-      return
+    try {
+      const videos = await channel.loadMore()
+      setVideos(videos)
+      setCanLoadMore(channel.canLoadMore())
+    } catch (err) {
+      setError(err)
+    } finally {
+      setLoading(false)
     }
-
-    if (res.items.length < 50) {
-      setCanLoadMore(false)
-      loadMoreCache[channel.id] = false;
-      localStorage.setItem('loadMoreCache', JSON.stringify(loadMoreCache))
-      return
-    }
-
-    channel.videos = videos.concat(res.items.slice(1))
-    setVideos(channel.videos)
-    yt.cacheAdd({ [channel.id]: channel })
   }
 
   React.useEffect(() => {
-    async function boot() {
-      try {
-        const channels = await yt.fetchChannels()
-        // if (channels.length) {
-        //   // TODO: fix duplication with `selectChannel` function
-        //   // getting a react error: "include it or remove the dependency array"
-        //   const channel = channels[0]
-        //   setChannel(channel)
-        //   setVideos(channel.videos)
-        //   updateLoadMore(channel)
-        // }
-        setChannels(channels)
-        // update "lastLoadCache" to track the last time the channel was fetched
-        channels.forEach(channel => {
-          if (lastLoadCache[channel.id] === undefined) {
-            lastLoadCache[channel.id] = Date.now()
-            localStorage.setItem('lastLoadCache', JSON.stringify(lastLoadCache))
-          }
-        })
-      } catch (exception) {
-        setError(exception)
-      }
-    }
-
-    boot()
+    yt.fetchChannels().then(setChannels).catch(setError)
   }, [])
 
   return (
--- src/Youtube.js
+++ src/Youtube.js
@@ -1,9 +1,65 @@
-const cacheVersion = '1'
-const cacheKey = 'ytsimple-channels'
-const cache = JSON.parse(localStorage.getItem(cacheKey) || '{}')
-export const cacheAdd = entry => setCache(Object.assign(cache, entry, { version: cacheVersion }))
-const setCache = cache => localStorage.setItem(cacheKey, JSON.stringify(cache))
-// if (cache.version != cacheVersion) setCache(cache = {})
+class Channel {
+  constructor(args) {
+    Object.assign(this, args)
+  }
+
+  async videos() {
+    await this.syncNewVideos()
+    return JSON.parse(localStorage.getItem(`${this.id}.videos`))
+  }
+
+  async syncNewVideos() {
+    const i = localStorage.getItem(`${this.id}.lastLoad`)
+    const lastLoad = i ? parseInt(i) : undefined
+    if (!lastLoad || (Date.now() - lastLoad) > 24 * 60 * 60 * 1000) {
+      const res = await request('/search', {
+        channelId: this.id,
+        order: 'date',
+        type: 'video',
+        maxResults: 50,
+        publishedAfter: new Date(lastLoad || 0).toISOString(),
+      })
+
+      if (res.error) throw res.error.message
+
+      localStorage.setItem(`${this.id}.lastLoad`, Date.now())
+      if (res.pageInfo.totalResults < 50) localStorage.setItem(`${this.id}.loadMore`, false)
+
+      this.updateVideos(this.loadVideos().concat(res.items))
+    }
+  }
+
+  async loadMore() {
+    const lastVideo = this.loadVideos().slice(-1)[0]
+    const res = await request('/search', {
+      channelId: this.id,
+      order: 'date',
+      type: 'video',
+      maxResults: 50,
+      // use `- 1e4` to fetch videos older than 10s of the current last video
+      publishedBefore: new Date(new Date(lastVideo.snippet.publishedAt).valueOf() - 1e4).toISOString(),
+    })
+
+    if (res.error) throw res.error.message
+
+    if (res.pageInfo.totalResults < 50) localStorage.setItem(`${this.id}.loadMore`, false)
+    this.updateVideos(this.loadVideos().concat(res.items))
+
+    return this.loadVideos()
+  }
+
+  canLoadMore() {
+    return localStorage.getItem(`${this.id}.loadMore`) !== 'false'
+  }
+
+  updateVideos(videos) {
+    localStorage.setItem(`${this.id}.videos`, JSON.stringify(videos))
+  }
+
+  loadVideos() {
+    return JSON.parse(localStorage.getItem(`${this.id}.videos`) || '[]')
+  }
+}
 
 async function request(path, args) {
   const url = new URL('https://www.googleapis.com/youtube/v3' + path)
@@ -18,38 +74,19 @@ async function request(path, args) {
 
 export async function fetchChannels() {
   const channels = await fetch(process.env.REACT_APP_CHANNELS_FILE).then(res => res.json())
-  const promises = channels.map(([id, _channelName]) => fetchChannelAndVideos(id))
+  const promises = channels.map(([id, _channelName]) => fetchChannel(id))
   return Promise.all(promises)
 }
 
-async function fetchChannelAndVideos(id) {
-  let cached = cache[id]
-  if (cached) return Promise.resolve(cached)
+async function fetchChannel(id) {
+  let cached = localStorage.getItem(id)
+  if (cached) return Promise.resolve(new Channel(JSON.parse(cached)))
 
-  const [channel, videos] = await Promise.all([
-    getChannel(id)
-      .then(res => { if (res.error) { throw res.error.message } else return res.items[0] }),
-    getChannelVideos(id)
-      .then(res => { if (res.error) { throw res.error.message } else return res.items })
-  ])
+  return request('/channels', { id }).then(res => {
+    if (res.error) throw res.error.message
 
-  channel.videos = videos
-  cacheAdd({ [id]: channel })
-
-  return channel
-}
-
-export function getChannel(id) {
-  return request('/channels', { id })
-}
-
-export function getChannelVideos(id, { before = undefined, after = undefined } = {}) {
-  return request('/search', {
-    channelId: id,
-    order: 'date',
-    type: 'video',
-    maxResults: 50,
-    ...(before ? { publishedBefore: before } : {}),
-    ...(after ? { publishedAfter: after } : {}),
+    const channel = res.items[0]
+    localStorage.setItem(id, JSON.stringify(channel))
+    return new Channel(channel)
   })
 }
